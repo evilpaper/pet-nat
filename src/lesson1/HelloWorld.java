1923d2f9d6e0dfdc12fcc24aa2047042885788af
Pelle Lundgren <pellburt@gmail.com>
2025-12-22 15:13:12 +0100

    refactor: move lesson two into its own class

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -3,8 +3,10 @@ package lesson1;
 public class HelloWorld {
 
     public static void run() {
+        System.out.println("--- lesson 1 - Hello World ---");
         System.out.println("Hello, world!");
         int x = 5;
         System.out.println(x);
+        System.out.println(" ");
     }
 }
