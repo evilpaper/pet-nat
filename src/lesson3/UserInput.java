3c4c19e3e6469457c721507c2539a74820d4ea64
Pelle Lundgren <pellburt@gmail.com>
2025-12-28 09:11:43 +0100

    feat: accept user input with scanner and use input to create an area calculator

--- src/lesson3/UserInput.java
+++ src/lesson3/UserInput.java
@@ -6,11 +6,20 @@ public class UserInput {
 
     public static void run() {
 
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is your name: ");
-        String name = scanner.next();
-        System.out.println("Hello " + name);
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
 
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area is: " + area);
     }
 }
